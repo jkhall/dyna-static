11eef39e4732a66c64dafe4c7f5deca0f19cdfc8
jkhall <jkhall@users.noreply.github.com>
2019-11-03 01:17:23 -0500

    commenting out

--- functions/datafn/datafn.js
+++ functions/datafn/datafn.js
@@ -22,56 +22,63 @@ exports.handler = async function(event, context, callback) {
       let sha = data.sha
       let decodedContent = JSON.parse(Buffer.from(content, 'base64').toString('ascii'))
       
-      console.log(`This is the sha: ${sha}`)
-      console.log(`This is the decoded content: ${decodedContent}`)
+      // console.log(`This is the sha: ${sha}`)
+      // console.log(`This is the decoded content: ${decodedContent}`)
 
-
-      // now make the post request for a new file with the appropriate headers
-      let putUrl = 'https://api.github.com/repos/jkhall/dyna-static/contents/data.json'
-
-      // according to the params in the request, update one of the data
-      // check if the name is valid
-      if(!decodedContent.data.map((v, i) => v.name).includes(name)){
-        if(!process.env.NETLIFY){
-          console.log("Name not found in data")
-          return
-        } else {
-          // callback({status: 400, body: "what the eff", message: "Name not found in data"})
-          return {
-            statusCode: 400,
-            body:{
-              message: "Name not found in data"
-            }
-          }
+      return {
+        statusCode: 200,
+        body: {
+          sha: sha,
+          data: decodedContent.data
         }
       }
+
+      // // now make the post request for a new file with the appropriate headers
+      // let putUrl = 'https://api.github.com/repos/jkhall/dyna-static/contents/data.json'
+
+      // // according to the params in the request, update one of the data
+      // // check if the name is valid
+      // if(!decodedContent.data.map((v, i) => v.name).includes(name)){
+      //   if(!process.env.NETLIFY){
+      //     console.log("Name not found in data")
+      //     return
+      //   } else {
+      //     // callback({status: 400, body: "what the eff", message: "Name not found in data"})
+      //     return {
+      //       statusCode: 400,
+      //       body:{
+      //         message: "Name not found in data"
+      //       }
+      //     }
+      //   }
+      // }
       
-      for(var node of decodedContent.data){
-        if(node.name === name){
-          node.done = "true"
-        }
-      }
+      // for(var node of decodedContent.data){
+      //   if(node.name === name){
+      //     node.done = "true"
+      //   }
+      // }
 
-      let newBody = {
-        message: "this is a test",
-        committer: {
-          name: "Jordan",
-          email: "jordankhall23@gmail.com"
-        },
-        content: Buffer.from(JSON.stringify(decodedContent)).toString('base64'),
-        sha: sha
-      }
+      // let newBody = {
+      //   message: "this is a test",
+      //   committer: {
+      //     name: "Jordan",
+      //     email: "jordankhall23@gmail.com"
+      //   },
+      //   content: Buffer.from(JSON.stringify(decodedContent)).toString('base64'),
+      //   sha: sha
+      // }
 
-      console.log(JSON.stringify(newBody))
+      // console.log(JSON.stringify(newBody))
 
-      fetch(putUrl, {method: 'PUT', body: JSON.stringify(newBody), headers: {Authorization: `token ${process.env.GITTOKEN}`}})
-        .then(res => res.json())
-        .then(jData => {
-          // callback({body: json})
-          return {
-            body: "What the eff",
-            payload: jData
-          }
-        })
+      // fetch(putUrl, {method: 'PUT', body: JSON.stringify(newBody), headers: {Authorization: `token ${process.env.GITTOKEN}`}})
+      //   .then(res => res.json())
+      //   .then(jData => {
+      //     // callback({body: json})
+      //     return {
+      //       body: "What the eff",
+      //       payload: jData
+      //     }
+      //   })
     })
 };
