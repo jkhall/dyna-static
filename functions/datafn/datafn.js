45dedea81ca11bd8c894531e23af77ebbc51ebc9
jkhall <jkhall@users.noreply.github.com>
2019-11-03 12:19:50 -0500

    more function debugging

--- functions/datafn/datafn.js
+++ functions/datafn/datafn.js
@@ -1,21 +1,14 @@
 // import fetch from 'node-fetch';
 let fetch = require('node-fetch')
 
-// if(!process.env.NETLIFY){
-//   require('dotenv').config()
-//   fetch = require('node-fetch')
-// }
+if(!process.env.NETLIFY){
+  require('dotenv').config()
+}
 
 exports.handler = async function(event, context) {
   // the name of the test data that we're trying to reference
-  // let name = event.queryStringParameters.name
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "maybe this'll finally work",
-      event: event
-    })
-  }
+  let name = event.queryStringParameters.name
+  
   // let name = "Bananas"
   // return name
   // console.log("poops before fetch")
@@ -25,17 +18,17 @@ exports.handler = async function(event, context) {
   //     message: `This stuff is ${name}`
   //   }
   // }))
-  // return fetch('https://api.github.com/repos/jkhall/dyna-static/contents/data.json')
-  //   .then(res => res.json())
-  //   .then(data => {
+  return fetch('https://api.github.com/repos/jkhall/dyna-static/contents/data.json')
+    .then(res => res.json())
+    .then(data => {
       // console.log(data.content)
 
-      // let content = data.content
-      // let sha = data.sha
-      // let decodedContent = JSON.parse(Buffer.from(content, 'base64').toString('ascii'))
+      let content = data.content
+      let sha = data.sha
+      let decodedContent = JSON.parse(Buffer.from(content, 'base64').toString('ascii'))
       
-      // console.log(`This is the sha: ${sha}`)
-      // console.log(`This is the decoded content: ${decodedContent}`)
+      console.log(`This is the sha: ${sha}`)
+      console.log(`This is the decoded content: ${decodedContent}`)
 
       // let payload = {
       //   statusCode: 200,
@@ -48,51 +41,65 @@ exports.handler = async function(event, context) {
       // return "poops"
 
       // // now make the post request for a new file with the appropriate headers
-      // let putUrl = 'https://api.github.com/repos/jkhall/dyna-static/contents/data.json'
+      let putUrl = 'https://api.github.com/repos/jkhall/dyna-static/contents/data.json'
 
       // // according to the params in the request, update one of the data
       // // check if the name is valid
-      // if(!decodedContent.data.map((v, i) => v.name).includes(name)){
-      //   if(!process.env.NETLIFY){
-      //     console.log("Name not found in data")
-      //     return
-      //   } else {
-      //     // callback({status: 400, body: "what the eff", message: "Name not found in data"})
-      //     return {
-      //       statusCode: 400,
-      //       body:{
-      //         message: "Name not found in data"
-      //       }
-      //     }
-      //   }
-      // }
+      if(!decodedContent.data.map((v, i) => v.name).includes(name)){
+        if(!process.env.NETLIFY){
+          console.log("Name not found in data")
+          return
+        } else {
+          // callback({status: 400, body: "what the eff", message: "Name not found in data"})
+          return {
+            statusCode: 400,
+            body: JSON.stringify({
+              message: "Name not found"
+            })
+          }
+        }
+      }
       
-      // for(var node of decodedContent.data){
-      //   if(node.name === name){
-      //     node.done = "true"
-      //   }
-      // }
+      for(var node of decodedContent.data){
+        if(node.name === name){
+          node.done = "true"
+        }
+      }
 
-      // let newBody = {
-      //   message: "this is a test",
-      //   committer: {
-      //     name: "Jordan",
-      //     email: "jordankhall23@gmail.com"
-      //   },
-      //   content: Buffer.from(JSON.stringify(decodedContent)).toString('base64'),
-      //   sha: sha
-      // }
+      let newBody = {
+        message: "this is a test",
+        committer: {
+          name: "Jordan",
+          email: "jordankhall23@gmail.com"
+        },
+        content: Buffer.from(JSON.stringify(decodedContent)).toString('base64'),
+        sha: sha
+      }
 
       // console.log(JSON.stringify(newBody))
 
-      // fetch(putUrl, {method: 'PUT', body: JSON.stringify(newBody), headers: {Authorization: `token ${process.env.GITTOKEN}`}})
-      //   .then(res => res.json())
-      //   .then(jData => {
-      //     // callback({body: json})
-      //     return {
-      //       body: "What the eff",
-      //       payload: jData
-      //     }
-      //   })
-    // })
+      fetch(putUrl, {method: 'PUT', body: JSON.stringify(newBody), headers: {Authorization: `token ${process.env.GITTOKEN}`}})
+        .then(res => res.json())
+        .then(jData => {
+          // callback({body: json})
+          return {
+            statusCode: 200,
+            body: JSON.stringify({
+              message: `${name} marked done!`
+            })
+          }
+        })
+        .catch(err => ({
+          statusCode: 400,
+          body: JSON.stringify({
+            message: err
+          })
+        }))
+    })
+    .catch(err => ({
+      statusCode: 400,
+      body: JSON.stringify({
+        message: err
+      })
+    }))
 };
